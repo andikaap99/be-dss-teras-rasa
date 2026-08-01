b7518a124f7ab0c65192c6cebf545ce40528e8e9
Andika Aryadi Putra <andford99@gmail.com>
2026-08-01 08:22:49 +0700

    fix: env

--- app/database/database.py
+++ app/database/database.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/dss_mie_ayam"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:@localhost/dss_mie_ayam"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
